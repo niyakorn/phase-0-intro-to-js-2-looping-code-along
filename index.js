0aaefb5eaf024252a3f81ad901771752209cae6d
Taya Niyakorn <tayawee@gmail.com>
2023-03-04 20:36:42 -0600

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+ function writeCards( names, event ) {
+  let appeciationCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    appeciationCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return appeciationCards
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
